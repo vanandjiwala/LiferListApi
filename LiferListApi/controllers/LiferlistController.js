d33b19448d233d651ea8470e059515a0807e6dcc
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2018-08-25 19:51:07 +0530

    clean up

--- LiferListApi/controllers/LiferlistController.js
+++ LiferListApi/controllers/LiferlistController.js
@@ -12,49 +12,49 @@ const getAllLifers = async function (req, res) {
 
         var myLifers;
 
-        if(categoryQry && speciesQry){
+        if (categoryQry && speciesQry) {
             console.log("both");
             myLifers = await Liferlist.findAll({
                 include: [
                     {
-                        model: Species, as: 'speciesId',required: true, attributes: ['name', 'scientificName', 'status'], where:{name : speciesQry},
-                        include: [{ model: Category, as: 'categoryId',required: true, attributes: ['categoryName'], where:{categoryName : categoryQry} }]
+                        model: Species, as: 'speciesId', required: true, attributes: ['name', 'scientificName', 'status'], where: { name: speciesQry },
+                        include: [{ model: Category, as: 'categoryId', required: true, attributes: ['categoryName'], where: { categoryName: categoryQry } }]
                     }
                 ]
             });
 
             return res.status(200).json(myLifers);
 
-        }else if(categoryQry){
+        } else if (categoryQry) {
 
             myLifers = await Liferlist.findAll({
                 include: [
                     {
-                        model: Species, as: 'speciesId',required: true, attributes: ['name', 'scientificName', 'status'],
-                        include: [{ model: Category, as: 'categoryId',required: true, attributes: ['categoryName'], where:{categoryName : categoryQry} }]
+                        model: Species, as: 'speciesId', required: true, attributes: ['name', 'scientificName', 'status'],
+                        include: [{ model: Category, as: 'categoryId', required: true, attributes: ['categoryName'], where: { categoryName: categoryQry } }]
                     }
                 ]
             });
 
             return res.status(200).json(myLifers);
 
-        }else if(speciesQry){
+        } else if (speciesQry) {
             myLifers = await Liferlist.findAll({
                 include: [
                     {
-                        model: Species, as: 'speciesId',required: true, attributes: ['name', 'scientificName', 'status'], where:{name : speciesQry},
-                        include: [{ model: Category, as: 'categoryId',required: true, attributes: ['categoryName'] }]
+                        model: Species, as: 'speciesId', required: true, attributes: ['name', 'scientificName', 'status'], where: { name: speciesQry },
+                        include: [{ model: Category, as: 'categoryId', required: true, attributes: ['categoryName'] }]
                     }
                 ]
             });
 
             return res.status(200).json(myLifers);
-        }else{
+        } else {
             myLifers = await Liferlist.findAll({
                 include: [
                     {
-                        model: Species, as: 'speciesId',required: true, attributes: ['name', 'scientificName', 'status'],
-                        include: [{ model: Category, as: 'categoryId',required: true, attributes: ['categoryName'] }]
+                        model: Species, as: 'speciesId', required: true, attributes: ['name', 'scientificName', 'status'],
+                        include: [{ model: Category, as: 'categoryId', required: true, attributes: ['categoryName'] }]
                     }
                 ]
             });
@@ -62,29 +62,8 @@ const getAllLifers = async function (req, res) {
             return res.status(200).json(myLifers);
         }
 
-       
-
     } catch (error) {
         return res.status(500).json({ "msg": "Something unexpected occured", "error": error });
     }
-    // let body = [];
-    // Liferlist.findAll({
-    //     include: [
-    //         {
-    //             model: Species,as: 'speciesId',attributes: ['name','scientificName','status'],
-    //             include:[{model: Category,as: 'categoryId',attributes: ['categoryName']}]
-    //         }
-    //     ]
-    // }).then(function(lifers){
-    //     //console.log(lifers[0].dataValues);
-    //     // for(var i = 0; i < lifers.length ; i++){
-    //     //     // console.log(lifers[i].dataValues.birdId.dataValues);
-    //     //     body.push(lifers[i].dataValues);
-    //     // }
-    //     //return res.json(JSON.stringify(body));
-    //     return res.json(lifers);
-    // }).catch(function(err){
-    //     return res.json({"msg":"Something unexpected occured","error":err})
-    // });
 }
 module.exports.getAllLifers = getAllLifers;
