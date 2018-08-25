6c5d6057ebfdf3cf75c90d8fd15e9610afae31c6
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2018-08-25 14:52:39 +0530

    Get all species API bug fixed

--- LiferListApi/controllers/SpeciesController.js
+++ LiferListApi/controllers/SpeciesController.js
@@ -4,27 +4,39 @@ const Category = require('../models').category;
 const getAllSpecies = async function (req, res) {
     res.setHeader("Content-Type", "application/json");
     var categoryQry = req.query.category;
-    console.log(categoryQry);
     var selectedCategory;
     try {
-        var category = await Category.findOne({
-            where: { categoryName: categoryQry },
-            attributes: ['id']
-        });
+        var category;
+        if(categoryQry){
+                category = await Category.findOne({
+                where: { categoryName: categoryQry },
+                attributes: ['id']
+            });
+        }
+
         if (category) {
             console.log(category.dataValues.id);
             selectedCategory = category.dataValues.id;
-        } else {
-            return res.status(400).json({ "msg": "Something unexpected occured" });
         }
+
     } catch (error) {
         return res.status(400).json({ "msg": "Something unexpected occured", "error": error });
     }
 
     try {
-        var species = await Species.findAll({
-            where: { category_id: selectedCategory }
-        });
+
+        var species;
+
+        if(selectedCategory){
+                species = await Species.findAll({
+                where: { category_id: selectedCategory }
+            });
+        }else{
+            species = await Species.findAll({
+                
+            });
+        }
+
         if (species) {
             return res.status(200).json(species);
         } else {
